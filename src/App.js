d2faa163083747ac43d0547a2ce2ec717ef8261b
Adrien Lemaire <ladrien2@gmail.com>
2018-12-05 23:33:55 +0100

    First version of the Hangman game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const LISTOFWORDS = ['BATEAU', 'ARBRE', 'TELEPHONE', 'ARMOIRE', 'PAPETERIE', 'VEHICULE', 'TROUSSEAU', 'CHOCOLAT', 'PISTOLET', 'MARMITE']
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      letters: this.createLetters(),
+      word: this.pickRandomWordFromTheArray(),
+      usedLetters: new Set(),
+    }
+  }
+
+  createLetters() {
+    const list = ALPHABET.split('')
+    return list
+  }
+
+  DoIWin() {
+    return !this.computeDisplay(this.state.word, this.state.usedLetters).split('').includes('_')
+  }
+
+  computeDisplay(phrase, usedLetters) {
+    return phrase.replace(/\w/g,
+      (letter) => (usedLetters.has(letter) ? letter : '_')
+    )
+  }
+
+  pickRandomWordFromTheArray() {
+    return LISTOFWORDS[Math.floor(Math.random() * LISTOFWORDS.length)]
+  }
+
+  handleLetterClick(letter) {
+    this.setState({
+      usedLetters: this.state.usedLetters.add(letter),
+      actualLetter: letter
+    })
+  }
+
+  getFeedbackforLetter(index) {
+    const { letters, usedLetters } = this.state
+    if (usedLetters.has(letters[index])) return 'alreadyUsed'
+    return 'neverUsed'
+  }
+
+  restart = () => {
+    console.log('aha')
+    this.setState({
+      letters: this.createLetters(),
+      word: this.pickRandomWordFromTheArray(),
+      usedLetters: new Set(),
+    })
+    console.log(this.state.word)
+  }
+
   render() {
+    const letters = this.state.letters
+    const won = this.DoIWin()
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="word">
+          {this.computeDisplay(this.state.word, this.state.usedLetters)}
+        </div>
+        {won ?
+          <div className="newGameBtn" onClick={this.restart}>Nouvelle partie</div>
+          :
+          <div className="letterList">
+            {letters.map((letter, index) =>
+              <div className={`letter ${this.getFeedbackforLetter(index)}`} key={index} onClick={() => this.handleLetterClick(letter)}>{letter}</div>
+            )}
+          </div>
+        }
+
+        {/* // {won && } */}
       </div>
     );
   }
