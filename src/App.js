be95caa53033f3a0081dacded06fa575308ceb6e
Adrien Lemaire <ladrien2@gmail.com>
2018-12-06 01:03:42 +0100

    Add a loose progression

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const LISTOFWORDS = ['BATEAU', 'ARBRE', 'TELEPHONE', 'ARMOIRE', 'PAPETERIE', 'VEHICULE', 'TROUSSEAU', 'CHOCOLAT', 'PISTOLET', 'MARMITE']
+const nbChances = 6
 
 class App extends Component {
 
@@ -12,6 +13,8 @@ class App extends Component {
       letters: this.createLetters(),
       word: this.pickRandomWordFromTheArray(),
       usedLetters: new Set(),
+      try: 0,
+      badTry: 0
     }
   }
 
@@ -24,8 +27,8 @@ class App extends Component {
     return !this.computeDisplay(this.state.word, this.state.usedLetters).split('').includes('_')
   }
 
-  computeDisplay(phrase, usedLetters) {
-    return phrase.replace(/\w/g,
+  computeDisplay(word, usedLetters) {
+    return word.replace(/\w/g,
       (letter) => (usedLetters.has(letter) ? letter : '_')
     )
   }
@@ -35,10 +38,18 @@ class App extends Component {
   }
 
   handleLetterClick(letter) {
-    this.setState({
-      usedLetters: this.state.usedLetters.add(letter),
-      actualLetter: letter
-    })
+    if(!this.state.usedLetters.has(letter)) {
+      if(!this.state.word.split('').includes(letter)) {
+        this.setState({
+          badTry: this.state.badTry+1
+        })
+      }
+      this.setState({
+        usedLetters: this.state.usedLetters.add(letter),
+        actualLetter: letter,
+        try: this.state.try+1
+      })
+    }
   }
 
   getFeedbackforLetter(index) {
@@ -57,16 +68,21 @@ class App extends Component {
     console.log(this.state.word)
   }
 
+  loosePercentage() {
+    return (this.state.badTry * 100)/nbChances
+  }
+
   render() {
     const letters = this.state.letters
     const won = this.DoIWin()
+    const loosePercentage = this.loosePercentage()+'%'
     return (
       <div className="App">
-        <div className="word">
+        <div className="word" style={{backgroundPositionY:loosePercentage}}>
           {this.computeDisplay(this.state.word, this.state.usedLetters)}
         </div>
         {won ?
-          <div className="newGameBtn" onClick={this.restart}>Nouvelle partie</div>
+          <div className="newGameBtn" onClick={this.restart}>Nouvelle partie}</div>
           :
           <div className="letterList">
             {letters.map((letter, index) =>
@@ -74,8 +90,6 @@ class App extends Component {
             )}
           </div>
         }
-
-        {/* // {won && } */}
       </div>
     );
   }
